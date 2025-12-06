606a2d1e1493bb093857165c3a1b4924d2c06034
k-akbari <khalil.akbari18@gmail.com>
2025-12-06 06:13:07 +0430

    update view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,7 @@
                 <h2>{{ $product->name }}</h2>
                 <p>{{ $product->description }}</p>
                 <p>Price: ${{ $product->price }}</p>
-                <p>Stock Quantity: {{ $product->stock_quantity }}</p>
+                <p>Stocks Quantity: {{ $product->stock_quantity }}</p>
                 <p>Saler: {{ $product->saler->name }}</p>
             </li>
         @endforeach
